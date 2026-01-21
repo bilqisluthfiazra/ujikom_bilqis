5b842f81a8d4e6148a8700e57410c120de8a6737
ini bilqisss <152266901+bilqisluthfiazra@users.noreply.github.com>
2026-01-21 16:50:30 +0700

    Update index.php

--- ujikom/index.php
+++ ujikom/index.php
@@ -1,11 +1,27 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sarana pengaduan</title>
+    <title>Sarana Pengaduan</title>
 </head>
 <body>
-    <h1> Selamat datang di Forum pengaduan sarana sekolah</h1>
+    <div>
+        <h1>SELAMAT DATANG</h1>
+        <h1>di Forum Pengaduan</h1>
+        <h1>SMK TI MUHAMMADIYAH CIKAMPEK</h1>
+
+        <div class="a">
+            <div class="b">
+                <a href="\ujikom\form_pengaduan.php">Form Pengaduan</a>
+            </div>
+            <div class="b">
+                <a href="\ujikom\cari_pengaduan.php">Cari Pengaduan</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
